333895c03a878e7836248b29ddcc1c0cafe0da9d
Éden  Ernandes <edenernandes@MacBook-Air-de-Eden.local>
2020-11-23 21:37:31 -0300

    Add relational algebra operators

--- queries_scripts.sql
+++ queries_scripts.sql
@@ -1,22 +1,58 @@
--- selecionar dados de uma tabela mediante alguma condição
--- selecionar um subconjunto de atributos de uma tabela
--- Retorna o nome, data de nascimento e CPF do indivíduo com CPF = "11111111111"
+/*
+    Descriçao da operaçao em Algebra Relacional:
+        π nomeCompleto, dataNascimento, CPF (σ CPF=11111111111(Pessoa))
+
+    Requisitos atendidos:
+        selecionar dados de uma tabela mediante alguma condição
+        selecionar um subconjunto de atributos de uma tabela 
+
+    Retorno:
+        O nome, data de nascimento e CPF do indivíduo com CPF = "11111111111"
+*/ 
 SELECT nomeCompleto, dataNascimento, CPF 
 FROM Pessoa 
 WHERE CPF = "11111111111";
 
--- selecionar todos os dados de uma tabela
--- Retorna todos os dados de Paciente
+/*
+    Descriçao da operaçao em Algebra Relacional: 
+        Paciente
+
+    Requisitos atendidos:
+        selecionar todos os dados de uma tabela
+
+    Retorno:
+        todos os dados de Paciente
+
+*/
 SELECT * 
 FROM Paciente;
 
--- realizar seleção de dados sem apresentar duplicadas
--- Retorna as especialidades dos enfermeiros cadastrados sem repetições
+/*
+    Descriçao da operaçao em Algebra Relacional:
+        π Especialidade (Enfermeiro)
+
+    Requisitos atendidos:
+        realizar seleção de dados sem apresentar duplicadas
+
+    Retorno:
+        as especialidades dos enfermeiros cadastrados sem repetições
+*/
 SELECT DISTINCT Especialidade
 FROM Enfermeiro;
 
 -- aplicar produto cartesiano
+-- atribuir apelidos
 -- Retorna o produto cartesiano a partir da tabela de Consulta e Prescreve
+
+/*  
+    NAO TA PRONTO
+    Descriçao da operaçao em Algebra Relacional:
+
+    Requisitos atendidos:
+
+    Retorno:
+
+*/
 SELECT 
     CPF_Paciente, registroMS
 FROM
@@ -24,14 +60,34 @@ FROM
         CROSS JOIN
     Prescreve AS b;
 
--- atribuir “apelidos”
--- Atribui apelido a coluna CPF_Paciente da tabela Acompanha
+
+/*
+    Descriçao da operaçao em Algebra Relacional:
+        ρ (paciente, CPF_Acompanhante) (Acompanha)
+
+    Requisitos atendidos:
+        atribuir “apelidos”
+
+    Retorno:
+        Atribui apelido a coluna CPF_Paciente da tabela Acompanha
+
+*/
 SELECT CPF_Paciente 
 AS paciente 
 FROM Acompanha;
 
--- aplicar união
--- Retorna o CRM dos medicos Clinico Geral e dos Psiquiatra
+
+/*
+    Descriçao da operaçao em Algebra Relacional:
+        π CRM (σ Especialidade='Clinico Geral' (Medico)) ∪ π CRM (σ Especialidade='Psiquiatra' (Medico))
+
+    Requisitos atendidos:
+        aplicar união
+
+    Retorno:
+        o CRM dos medicos Clinico Geral e dos Psiquiatra
+
+*/
 SELECT CRM 
 FROM Medico 
 WHERE Especialidade = 'Clinico Geral' 
@@ -39,10 +95,26 @@ UNION SELECT CRM
 FROM Medico 
 WHERE Especialidade = 'Psiquiatra';
 
--- aplicar intersecao
--- atribuir "apelidos"
--- realizar seleção sob condição que envolva outra seleção
--- Retorna os nomes das pessoas que sao funcionarios, ps: INTERSECT nao e suportado pelo MySQL
+
+/*
+    NAO TA PRONTO
+    Descriçao da operaçao em Algebra Relacional:
+        CPF_FUNCIONARIO ← π CPF_Funcionario (Funcionario)
+        ρ(CPF) CPF_FUNCIONARIO
+        NOME_COMPLETO ← π nomeCompleto (CPF_FUNCIONARIO)
+
+
+
+    Requisitos atendidos:
+        aplicar intersecao
+        atribuir "apelidos"
+        realizar seleção sob condição que envolva outra seleção
+
+    Retorno:
+        nomes das pessoas que sao funcionarios
+        ps: INTERSECT nao e suportado pelo MySQL
+
+*/
 SELECT nomeCompleto AS Nome
 FROM Pessoa
 WHERE CPF IN (
@@ -52,15 +124,35 @@ FROM Funcionario);
 -- aplicar diferenca
 -- realizar seleção sob condição que envolva outra seleção
 -- Retorna  as matriculas dos funcionarios que nao sao pacientes do hospital, ps: EXCEPT nao e suportado pelo MySQL
+/*
+    NAO TA PRONTO
+    Descriçao da operaçao em Algebra Relacional:
+
+
+    Requisitos atendidos:
+
+    Retorno:
+
+*/
 SELECT matricula
 FROM Funcionario
 WHERE matricula NOT IN (
     SELECT matricula_funcionario
     FROM Funcionario_Paciente);
 
--- ordenar resultados de consultas de forma ascendente
--- realizar seleção sob condição que envolva outra seleção
--- ordena os funcionarios do hospital em ordem alfabetica
+
+/*
+    Descriçao da operaçao em Algebra Relacional:
+        NAO SE APLICA
+
+    Requisitos atendidos:
+        ordenar resultados de consultas de forma ascendente
+        realizar seleção sob condição que envolva outra seleção
+
+    Retorno:
+        Ordena os funcionarios do hospital em ordem alfabetica
+
+*/
 SELECT nomeCompleto AS Nome
 FROM Pessoa
 WHERE CPF IN (
@@ -68,27 +160,64 @@ SELECT CPF_Funcionario AS CPF
 FROM Funcionario) 
 ORDER BY Nome ASC;
 
--- consultar valores de atributos que iniciem por algum termo
--- Retorna pessoas cujo nome comeca por "Robert"
+/*
+    Descriçao da operaçao em Algebra Relacional:
+        NAO SE APLICA
+
+    Requisitos atendidos:
+        consultar valores de atributos que iniciem por algum termo
+
+    Retorno:
+        pessoas cujo nome comeca por "Robert"
+
+*/
 SELECT nomeCompleto
 FROM Pessoa
 WHERE nomeCompleto LIKE "Robert%";
 
+/*
+    Descriçao da operaçao em Algebra Relacional:
+        NAO SE APLICA
 
--- realizar agrupamento de resultado de consulta
--- atribuir "apelidos"
--- utilizar função de agregação
--- Retorna a quantidade de Medicos e agrupa-os por Especialidade
+    Requisitos atendidos:
+        realizar agrupamento de resultado de consulta
+        atribuir "apelidos"
+        utilizar função de agregação
+
+    Retorno:
+        a quantidade de Medicos e agrupa-os por Especialidade
+
+*/
 SELECT Especialidade, count(CRM) AS Qtde 
 FROM Medico 
 GROUP BY Especialidade;
 
--- realizar consulta de seleção-projeção-junção
+/*
+    NAO TA PRONTO
+    Descriçao da operaçao em Algebra Relacional:
+
+    Requisitos atendidos:
+        realizar consulta de seleção-projeção-junção
+
+    Retorno:
+
+*/
 SELECT data_admissao, CNTPS 
 FROM Funcionario, Medico
 WHERE salario > 2000 AND matricula=matricula_medico;
 
--- criar uma transação única
+/*
+    Descriçao da operaçao em Algebra Relacional:
+        NAO SE APLICA
+
+    Requisitos atendidos:
+        criar uma transação única
+
+    Retorno:
+        Executa uma transaçao setando data_saida de Paciente para AGORA
+        para determinados pacientes.
+
+*/
 START TRANSACTION;
     UPDATE Paciente SET data_saida = NOW()
     WHERE CPF_Paciente="12925378901";
@@ -96,7 +225,17 @@ START TRANSACTION;
     WHERE CPF_Paciente="12745987901";
 COMMIT;
 
--- CRIAÇÃO DE VIEW
+/*
+    Descriçao da operaçao em Algebra Relacional:
+        NAO SE APLICA
+
+    Requisitos atendidos:
+        criar uma view a partir de mais de uma tabela
+
+    Retorno:
+        view
+
+*/
 CREATE VIEW paciente_sem_enfermeiro AS
 SELECT CPF_Paciente
 FROM Paciente
@@ -106,24 +245,54 @@ WHERE CPF_Paciente NOT IN (
 
 SELECT * FROM paciente_sem_enfermeiro;
 
--- criar usuário que possua a regra padrão para consulta da view (criadas anteriormente)
--- criar uma regra para consulta apenas na visualização criada
+/*
+    Descriçao da operaçao em Algebra Relacional:
+        NAO SE APLICA
+
+    Requisitos atendidos:
+        criar usuário que possua a regra padrão para consulta da view (criadas anteriormente)
+        criar uma regra para consulta apenas na visualização criada
+
+    Retorno:
+        NAO SE APLICA
+
+*/
 CREATE ROLE reader_view;
 GRANT SELECT ON hospital.paciente_sem_enfermeiro TO 'reader_view';
 CREATE USER 'viewer'@'localhost' IDENTIFIED BY 'viewer';
 GRANT 'reader_view' TO 'viewer'@'localhost';
 SET DEFAULT ROLE 'reader_view' TO 'viewer'@'localhost';
 
--- criar regra apenas para consultas do banco criado
--- criar usuário que possua a regra padrão para consulta (criada anteriormente)
+/*
+    Descriçao da operaçao em Algebra Relacional:
+        NAO SE APLICA
+
+    Requisitos atendidos:
+        criar regra apenas para consultas do banco criado
+        criar usuário que possua a regra padrão para consulta (criada anteriormente)
+
+    Retorno:
+        NAO SE APLICA
+
+*/
 CREATE ROLE role_for_select;
 GRANT SELECT ON hospital.* TO 'role_for_select';
 CREATE USER 'user_only_for_select'@'localhost' IDENTIFIED BY 'select';
 GRANT 'role_for_select' TO 'user_only_for_select'@'localhost';
 SET DEFAULT ROLE 'role_for_select' TO 'user_only_for_select'@'localhost';
 
--- criar regra para execução de comandos DDL
--- criar usuário que possua a regra padrão para comandos DDL (criada anteriormente)
+/*
+    Descriçao da operaçao em Algebra Relacional:
+        NAO SE APLICA
+
+    Requisitos atendidos:
+        criar regra para execução de comandos DDL
+        criar usuário que possua a regra padrão para comandos DDL (criada anteriormente)
+
+    Retorno:
+        NAO SE APLICA
+
+*/
 CREATE ROLE role_for_ddl;
 GRANT CREATE ON hospital.* TO 'role_for_ddl';
 GRANT ALTER ON hospital.* TO 'role_for_ddl';
@@ -132,8 +301,18 @@ CREATE USER 'user_ddl'@'localhost' IDENTIFIED BY 'ddl';
 GRANT 'role_for_ddl' TO 'user_ddl'@'localhost';
 SET DEFAULT ROLE 'role_for_ddl' TO 'user_ddl'@'localhost';
 
--- criar usuário que possua a regra padrão para comandos DML e DQL (criada anteriormente)
--- criar regra para execução de comandos DML e DQL
+/*
+    Descriçao da operaçao em Algebra Relacional:
+        NAO SE APLICA
+
+    Requisitos atendidos:
+        criar regra para execução de comandos DML e DQL
+        criar usuário que possua a regra padrão para comandos DML e DQL (criada anteriormente)
+      
+    Retorno:
+        NAO SE APLICA
+
+*/
 CREATE ROLE role_for_dml_dql;
 GRANT INSERT ON hospital.* TO 'role_for_dml_dql';
 GRANT UPDATE ON hospital.* TO 'role_for_dml_dql';
